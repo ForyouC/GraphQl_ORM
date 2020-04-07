6b40727e02e344f069af53c7fda55b883cad7bc1
Foryou <hjw7548@gmail.com>
2020-04-08 02:16:15 +0900

    second

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,7 +1,30 @@
-function sum(a,b) {
-    return a + b;
-}
+import { request } from "graphql-request";
+import { host } from "./constants";
+import { User } from "../entity/User";
+import { createTypeormConn } from "../utils/createTypeormConn";
 
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1,2)).toBe(3);
+beforeAll(async () => {
+  await createTypeormConn();
 });
+
+const email = "tom@bob.com";
+const password = "jalksdf";
+
+const mutation = `
+mutation {
+  register(email: "${email}", password: "${password}")
+}
+`;
+
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
+// use a test database
+// drop all data once the test is over
+// when i run npm yarn test it also starts the server

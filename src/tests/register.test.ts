001573d6743523a250d05a030b92a49a864ed22d
Foryou <hjw7548@gmail.com>
2020-04-08 02:18:51 +0900

    good

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,30 +1,31 @@
-import { request } from "graphql-request";
-import { host } from "./constants";
+import { request } from 'graphql-request';
+import { host } from './constants';
 import { User } from "../entity/User";
-import { createTypeormConn } from "../utils/createTypeormConn";
+import { createTypeormConn } from '../utils/createTypeormConn';
 
 beforeAll(async () => {
-  await createTypeormConn();
-});
+    await createTypeormConn();
+})
 
-const email = "tom@bob.com";
-const password = "jalksdf";
+const email = "hjk14985@naver.com";
+const password = "sky14985";
 
 const mutation = `
 mutation {
-  register(email: "${email}", password: "${password}")
+    register(email: "${email}", password: "${password}")
 }
 `;
 
 test("Register user", async () => {
-  const response = await request(host, mutation);
-  expect(response).toEqual({ register: true });
-  const users = await User.find({ where: { email } });
-  expect(users).toHaveLength(1);
-  const user = users[0];
-  expect(user.email).toEqual(email);
-  expect(user.password).not.toEqual(password);
+    const response = await request(host, mutation);
+    expect(response).toEqual({ register: true });
+    const users = await User.find({ where: { email } });
+    expect(users).toHaveLength(1);
+    const user = users[0];
+    expect(user.email).toEqual(email);
+    expect(user.password).not.toEqual(password);
 });
+
 // use a test database
 // drop all data once the test is over
 // when i run npm yarn test it also starts the server
